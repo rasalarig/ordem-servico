87005388100c3b9ca20fb4d5fbbbfbc527551c2c
hailtonnegocios <hailtonnegocios@gmail.com>
2014-05-19 15:58:43 -0300

    Update AutorizadorInterceptor.java
    Teste Merge Hailton

--- src/br/com/iniciativa21/ordemservico/interceptor/AutorizadorInterceptor.java
+++ src/br/com/iniciativa21/ordemservico/interceptor/AutorizadorInterceptor.java
@@ -10,6 +10,7 @@ import br.com.iniciativa21.ordemservico.model.entity.Usuario;
 
 public class AutorizadorInterceptor extends HandlerInterceptorAdapter{
 	
+	// teste merge
 	@Override
 	public boolean preHandle(HttpServletRequest request,HttpServletResponse response,Object controller)
 	throws Exception{
